cf44db79000a85e5bbaf453c91b4b56eda834e20
Jan Redmann <janredmann1@gmail.com>
2020-02-05 16:41:59 +0100

    fix bug image upload

--- client/src/components/ProjectForm.js
+++ client/src/components/ProjectForm.js
@@ -10,8 +10,7 @@ class ProjectForm extends Component {
     description: "",
     imageURL: "",
     publicID: "",
-    submitted: false,
-    imageSelected: false,
+    file: ""
   };
 
   handleChange = event => {
@@ -20,68 +19,94 @@ class ProjectForm extends Component {
     });
   };
 
-  handleFileUpload = e => {
-    console.log("The file to be uploaded is: ", e.target.files[0]);
+  handleFile = event => {
+    const file = event.target.files[0];
     this.setState({
-      imageSelected: true
+      file: file
     });
-    const uploadData = new FormData();
-    uploadData.append("imageURL", e.target.files[0]);
-
-    service.handleUpload(uploadData)
-      .then(response => {
-        const imageURL = response.secure_url;
-        const publicID = response.public_id;
-        console.log('res from handleupload: ', response.secure_url);
-        this.setState({ imageURL: imageURL, publicID: publicID });
-        console.log('new state: ', this.state.imageURL);
-        // check if the form already got submitted and only waits for the image upload
-        if (this.state.submitted === true) {
-          this.handleSubmit();
-        }
-      })
-      .catch(err => {
-        this.setState({
-          imageSelected: false
-        });
-        console.log("Error while uploading the file: ", err);
-      });
   }
 
-  handleSubmit = event => {
-    if (event) {
-      event.preventDefault();
-    }
-    console.log("SUBMIT");
-    // check if the image is already uploaded to the cloud or no image was selected
-    if (this.state.imageURL || !this.state.imageSelected) {
-      // axios.post('http://localhost:5555/api/projects')
-      axios
-        .post("/api/projects", {
-          title: this.state.title,
-          description: this.state.description,
-          imageURL: this.state.imageURL,
-          imagePublicID: this.state.publicID
-        })
-        .then(response => {
-          this.props.refreshData();
-          this.setState({
-            title: "",
-            description: "",
-            imageURL: "",
-            publicID: ""
-          });
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    } else {
-      // set a flag that the project got submitted
+  handleSubmit = async event => {
+    event.preventDefault();
+
+    console.log('handleSubmit: ', this.state.file);
+
+    const uploadData = new FormData();
+    uploadData.append("imageURL", this.state.file);
+
+    try {
+      const cloudinaryResponse = await service.handleUpload(uploadData);
+      const imageURL = cloudinaryResponse.secure_url;
+      const publicID = cloudinaryResponse.public_id;
+      this.setState({ imageURL: imageURL, publicID: publicID });
+    } catch (err) {
       this.setState({
-        submitted: true
+        error: err
       })
     }
-  };
+
+    try {
+      const response = await axios.post("/api/projects", {
+        title: this.state.title,
+        description: this.state.description,
+        imageURL: this.state.imageURL,
+        imagePublicID: this.state.publicID
+      })
+    } catch (err) {
+      this.setState({
+        error: err
+      })
+    }
+
+    this.props.refreshData();
+    this.setState({
+      title: "",
+      description: "",
+      imageURL: "",
+      publicID: ""
+    });
+  }
+
+
+
+
+  // service.handleUpload(uploadData)
+  //   .then(response => {
+  //     const imageURL = response.secure_url;
+  //     const publicID = response.public_id;
+  //     this.setState({ imageURL: imageURL, publicID: publicID });
+  //     console.log('new state: ', this.state);
+  //   })
+  //   .catch(err => {
+  //     this.setState({
+  //       imageSelected: false
+  //     });
+  //     console.log("Error while uploading the file: ", err);
+  //     this.setState({
+  //       error: err
+  //     })
+  //   });
+  // console.log('before axios: ', this.state.imageURL);
+  // axios
+  //   .post("/api/projects", {
+  //     title: this.state.title,
+  //     description: this.state.description,
+  //     imageURL: this.state.imageURL,
+  //     imagePublicID: this.state.publicID
+  //   })
+  //   .then(response => {
+  //     this.props.refreshData();
+  //     this.setState({
+  //       title: "",
+  //       description: "",
+  //       imageURL: "",
+  //       publicID: ""
+  //     });
+  //   })
+  //   .catch(err => {
+  //     console.log(err);
+  //   });
+  // };
 
   render() {
     return (
@@ -114,7 +139,7 @@ class ProjectForm extends Component {
             type="file"
             name="image"
             id="image"
-            onChange={this.handleFileUpload}
+            onChange={this.handleFile}
           />
         </Form.Group>
 
